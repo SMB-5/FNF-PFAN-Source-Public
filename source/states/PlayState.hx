f323447d7cb5303c5a408b130d2e1625d0995313
SMB <123291842+SMB-5@users.noreply.github.com>
2024-04-22 08:24:57 +0800

    dorm stage coded in

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -391,6 +391,8 @@ class PlayState extends MusicBeatState
 			case 'school': new states.stages.School(); //Week 6 - Senpai, Roses
 			case 'schoolEvil': new states.stages.SchoolEvil(); //Week 6 - Thorns
 			case 'tank': new states.stages.Tank(); //Week 7 - Ugh, Guns, Stress
+
+			case 'Dorm': new states.stages.Dorm();
 		}
 
 		if(isPixelStage) {
---
+++ source/states/stages/Dorm.hx
@@ -0,0 +1,14 @@
+package states.stages;
+
+import states.stages.objects.*;
+
+class Dorm extends BaseStage
+{
+
+	override function create()
+	{
+		var bg:BGSprite = new BGSprite('persona/stages/dorm/dorm', -600, -300);
+		add(bg);
+	}
+
+}
