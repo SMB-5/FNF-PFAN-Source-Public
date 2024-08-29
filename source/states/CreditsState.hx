1dba65789cbeb3fcd739ffbd0b821570ebae12da
SMB <123291842+SMB-5@users.noreply.github.com>
2024-08-29 13:48:24 +0800

    Updated dude's credit link

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -43,7 +43,7 @@ class CreditsState extends MusicBeatState
 			['SMB', 'SMB', 'Director and Main Programmer\n"Hey thank you so much for playing, I hope you enjoyed the mod as much as we did making it."', 'https://smb-bio.carrd.co/', 'EC1C26'],
 			['Clefanight', 'clefa', 'Artist/Animator\n"How the fuck did I even get here."', 'https://cremiesilver.newgrounds.com/', '11e92b'],
 			['Bro is that Dog', 'doggo', 'Musician\n"Hi! I composed a few songs for le epic persona mod, hope you enjoyed listening to them! (also please subscribe to my youtube come on im starving for subs please im begging you ple)"', 'https://www.youtube.com/@you-know-its-dog.', 'f5d79d'],
-			['DudeDX', '', 'Charter\n"It\'s not overcharted you\'re just bad."', 'https://twitter.com/dudeDX819', '5B9A4C'],
+			['DudeDX', '', 'Charter\n"It\'s not overcharted you\'re just bad."', 'https://bsky.app/profile/dudedx.bsky.social', '5B9A4C'],
 			['LarryFrosty', 'baldiplier', 'Charter and Assisted with Coding\n"Make sure to keep your pliers bald."', 'https://youtube.com/@larryfrosty?si=HBFHaclYVeS6kn8v', '52a5eb'],
 			['NoahGani1', 'noah', 'Chromatic Maker\n"Say Gex."', 'https://x.com/noah_gani1', '203a53'],
 			[''],
