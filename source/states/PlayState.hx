eb0c54dbba3e8a10aa393e7c0477d92b943fe6ea
SMB <123291842+SMB-5@users.noreply.github.com>
2024-11-13 16:46:23 +0800

    meta card in front of the notes so people can read

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -640,16 +640,6 @@ class PlayState extends MusicBeatState
 		updateScore(false);
 		uiGroup.add(scoreTxt);
 
-		hasMetadata = (metadata != null);
-
-		if (hasMetadata) {
-			card = new SongCard(0, 0, metadata);
-			card.screenCenter(Y);
-			card.x = -card.width;
-			uiGroup.add(card);
-			setOnScripts('card', card);
-		}
-
 		botplayTxt = new FlxText(400, healthBar.y - 90, FlxG.width - 800, "BOTPLAY", 32);
 		botplayTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		botplayTxt.scrollFactor.set();
@@ -662,6 +652,17 @@ class PlayState extends MusicBeatState
 		uiGroup.cameras = [camHUD];
 		noteGroup.cameras = [camHUD];
 		comboGroup.cameras = [camHUD];
+
+		hasMetadata = (metadata != null);
+
+		if (hasMetadata) {
+			card = new SongCard(0, 0, metadata);
+			card.screenCenter(Y);
+			card.x = -card.width;
+			add(card);
+			card.cameras = [camHUD];
+			setOnScripts('card', card);
+		}
 
 		startingSong = true;
 
