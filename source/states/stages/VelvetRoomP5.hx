d8c0f42d43d534680ad56b2e177bf41bf7ab4fa5
SMB <123291842+SMB-5@users.noreply.github.com>
2025-08-09 12:28:31 +0800

    do we need shadows? + small p5 velvet room edits

--- source/states/stages/VelvetRoomP5.hx
+++ source/states/stages/VelvetRoomP5.hx
@@ -6,10 +6,15 @@ class VelvetRoomP5 extends BaseStage
 {
 	override function create()
 	{
-		var room:BGSprite = new BGSprite('persona/stages/velvet-room-p5/Room', -600, -300);
+		var room:BGSprite = new BGSprite('persona/stages/velvet-room-p5/Room', -600, -300, 0.8, 0.8);
 		add(room);
 
-		var desk:BGSprite = new BGSprite('persona/stages/velvet-room-p5/Desk', -600, -300);
+		if(!ClientPrefs.data.lowQuality) {
+			var deskshadow:BGSprite = new BGSprite('persona/stages/velvet-room-p5/Shadow-Desk', -600, -300, 0.9, 0.9);
+			add(deskshadow);
+		}
+
+		var desk:BGSprite = new BGSprite('persona/stages/velvet-room-p5/Desk', -600, -300, 0.9, 0.9);
 		add(desk);
 	}
 
