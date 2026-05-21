0c16d266c7dac77233500201f3124ce440713b25
LarryFrosty <larrydobefrosty@gmail.com>
2026-05-21 09:05:51 +0800

    g

--- source/objects/StrumNote.hx
+++ source/objects/StrumNote.hx
@@ -24,7 +24,7 @@ class StrumNote extends FlxSprite
 		return value;
 	}
 
-	public var defaultRGB:Array<Array<FlxColor>> = PlayState.isPixelStage ? ClientPrefs.data.arrowRGBPixel ? ClientPrefs.data.arrowRGB;
+	public var defaultRGB:Array<Array<FlxColor>> = PlayState.isPixelStage ? ClientPrefs.data.arrowRGBPixel : ClientPrefs.data.arrowRGB;
 
 	public var useRGBShader:Bool = true;
 	public function new(x:Float, y:Float, leData:Int, player:Int) {
