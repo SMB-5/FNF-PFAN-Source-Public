0e5fbeb48f219701bba205baa4e52283c5663e57
melodiekit <melodiekitt@gmail.com>
2026-07-05 23:17:28 +0800

    New pause menu transition

--- source/substates/PauseSubState.hx
+++ source/substates/PauseSubState.hx
@@ -31,8 +31,12 @@ class PauseSubState extends MusicBeatSubstate
 	var buttonHitboxes:Array<FlxObject> = [];
 	var buttonsBG:FlxSprite;
 	var buttonsBG2:FlxSprite;
-	var back:FlxSprite;
-	var blocks:FlxSprite;
+	var back1:FlxSprite;
+	var back2:FlxSprite;
+	var back3:FlxSprite;
+	var block1:FlxSprite;
+	var block2:FlxSprite;
+	var block3:FlxSprite;
 
 	var tweens:Array<FlxTween> = [];
 
@@ -54,23 +58,43 @@ class PauseSubState extends MusicBeatSubstate
 		bg.scrollFactor.set();
 		add(bg);
 
-		back = new FlxSprite(0, 0).loadGraphic(Paths.image('persona/menus/pause/back'));
-		back.antialiasing = ClientPrefs.data.antialiasing;
-		add(back);
+		back1 = new FlxSprite(0, 0, Paths.image('persona/menus/pause/bg1'));
+		back1.antialiasing = ClientPrefs.data.antialiasing;
+		add(back1);
 
-		buttonsBG = new FlxSprite(0, 110).loadGraphic(Paths.image('persona/menus/pause/buttonsback'));
+		back2 = new FlxSprite(0, 0, Paths.image('persona/menus/pause/bg2'));
+		back2.antialiasing = ClientPrefs.data.antialiasing;
+		back2.x = FlxG.width - back2.width;
+		add(back2);
+
+		back3 = new FlxSprite(0, 0, Paths.image('persona/menus/pause/bg3'));
+		back3.antialiasing = ClientPrefs.data.antialiasing;
+		back3.x = FlxG.width - back3.width;
+		back3.y = FlxG.height - back3.height;
+		add(back3);
+
+		block1 = new FlxSprite(0, 0, Paths.image('persona/menus/pause/block1'));
+		block1.antialiasing = ClientPrefs.data.antialiasing;
+		add(block1);
+
+		block2 = new FlxSprite(0, 0, Paths.image('persona/menus/pause/block2'));
+		block2.antialiasing = ClientPrefs.data.antialiasing;
+		block2.x = FlxG.width - block2.width;
+		add(block2);
+
+		block3 = new FlxSprite(0, 0, Paths.image('persona/menus/pause/block3'));
+		block3.antialiasing = ClientPrefs.data.antialiasing;
+		block3.x = FlxG.width - block3.width;
+		block3.y = FlxG.height - block3.height;
+		add(block3);
+
+		buttonsBG = new FlxSprite(0, 110, Paths.image('persona/menus/pause/buttonsback'));
 		buttonsBG.antialiasing = ClientPrefs.data.antialiasing;
 		add(buttonsBG);
 
-		buttonsBG2 = new FlxSprite(50, 135).loadGraphic(Paths.image('persona/menus/pause/buttonsback2'));
+		buttonsBG2 = new FlxSprite(50, 135, Paths.image('persona/menus/pause/buttonsback2'));
 		buttonsBG2.antialiasing = ClientPrefs.data.antialiasing;
-		buttonsBG2.origin.set(buttonsBG2.width, buttonsBG2.height);
-		buttonsBG2.angle = -75;
 		add(buttonsBG2);
-
-		blocks = new FlxSprite(0, 0).loadGraphic(Paths.image('persona/menus/pause/blocks'));
-		blocks.antialiasing = ClientPrefs.data.antialiasing;
-		add(blocks);
 
 		var levelInfo:FlxText = new FlxText(35, 0, 0, PlayState.SONG.song + ' - Unknown', 24);
 		levelInfo.scrollFactor.set();
@@ -115,12 +139,28 @@ class PauseSubState extends MusicBeatSubstate
 		descTxt.alpha = 0;
 		add(descTxt);
 
+		back1.x -= back1.width;
+		back2.x += back2.width;
+		back3.x += back3.width;
+		block1.x -= block1.width;
+		block2.x += block2.width;
+		block3.x += block3.width;
+		buttonsBG.y += FlxG.height;
+		buttonsBG2.y += FlxG.height;
+
 		startTween(bg, {alpha: 0.6}, 0.4, {ease: FlxEase.quartInOut});
-		startTween(buttonsBG2, { angle: 0, x: 10 }, 0.75, { ease: FlxEase.quintOut });
+		startTween(back1, {x: back1.x + back1.width}, 0.5, {ease: FlxEase.cubeInOut});
+		startTween(back2, {x: back2.x - back2.width}, 0.5, {ease: FlxEase.cubeInOut, startDelay: 0.15});
+		startTween(back3, {x: back3.x - back3.width}, 0.5, {ease: FlxEase.cubeInOut, startDelay: 0.3});
+		startTween(block1, {x: block1.x + block1.width}, 0.5, {ease: FlxEase.cubeInOut, startDelay: 0.15});
+		startTween(block2, {x: block2.x - block2.width}, 0.5, {ease: FlxEase.cubeInOut, startDelay: 0.3});
+		startTween(block3, {x: block3.x - block3.width}, 0.5, {ease: FlxEase.cubeInOut, startDelay: 0.45});
+		startTween(buttonsBG, {y: buttonsBG.y - FlxG.height}, 0.75, {ease: FlxEase.quintOut});
+		startTween(buttonsBG2, {y: buttonsBG2.y - FlxG.height}, 0.75, {ease: FlxEase.quintOut, startDelay: 0.05});
 		startTween(levelInfo, {alpha: 1, y: levelInfo.y + 5}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.3});
 		startTween(descTxt, {alpha: 1, y: descTxt.y + 5}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.7});
-		startTween(creditArt, {alpha: 1, y: creditArt.y + 5}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.3});
-		startTween(creditCode, {alpha: 1, y: creditCode.y + 5}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.5});
+		startTween(creditArt, {alpha: 1, y: creditArt.y + 5}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.55});
+		startTween(creditCode, {alpha: 1, y: creditCode.y + 5}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.55});
 		startTween(creditChart, {alpha: 1, y: creditChart.y + 5}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.5});
 
 		pauseButtons = new FlxTypedGroup<FlxSprite>();
@@ -172,11 +212,8 @@ class PauseSubState extends MusicBeatSubstate
 			add(hitbox);
 			buttonHitboxes.push(hitbox);
 
-			pauseButton.origin.set(pauseButton.width, pauseButton.height);
-			pauseButton.angle = -75;
-			pauseButton.x -= 210;
-			pauseButton.y += 390;
-			startTween(pauseButton, { angle: 0, x: pauseButton.x + 210, y: pauseButton.y - 390 }, 0.75, { ease: FlxEase.quintOut });
+			pauseButton.y += FlxG.height;
+			startTween(pauseButton, { y: pauseButton.y - FlxG.height }, 0.75, { ease: FlxEase.quintOut, startDelay: 0.1 * (i + 1) });
 		}
 
 		changeSelection();
@@ -295,6 +332,7 @@ class PauseSubState extends MusicBeatSubstate
 		for (tween in tweens) tween.cancel();
 		pauseMusic.destroy();
 		#if !mobile FlxG.mouse.visible = false; #end
+		FlxG.inputs.reset();
 
 		super.destroy();
 	}
